80b1e1bfaf9c781ee243c57948a3ab179577b4ac
Sunny Agrawal <agrawalsunny557@gmail.com>
2020-05-31 23:20:36 +0530

    RxJs Part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -54,6 +55,7 @@ import { LoginComponent } from './login/login.component'
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
+    MatProgressSpinnerModule,
     MatInputModule,
     MatCheckboxModule,
     MatCardModule,
